e835354c238abd6f773c1321017f68b9ae4a48ae
SebWae <s254120@dtu.dk>
2025-11-09 16:39:27 +0100

    feat: testing multiple model types

--- perform_tlcv.py
+++ perform_tlcv.py
@@ -3,8 +3,10 @@ import sklearn.linear_model as lm
 
 from utils import two_layer_cv, BaselineRegressor, ANNRegressor, Preprocessor
 
-models = [BaselineRegressor]
-
+models_list = [[BaselineRegressor()], 
+               [lm.Ridge(alpha=14)],
+               [lm.Ridge(alpha=15)],
+               ]
 
 df = pd.read_csv("data/heartDisease.csv")
 
@@ -12,10 +14,10 @@ PreProp = Preprocessor(task="regression")
 PreProp.fit(df)
 X, y = PreProp.transform(df)
 
-
-two_layer_cv(k_in=10,
-             k_out=10,
-             models=models,
-             X=X,
-             y=y,
-             mode="regression")
+for models in models_list:
+    two_layer_cv(k_in=10,
+                k_out=10,
+                models=models,
+                X=X,
+                y=y,
+                mode="regression")
