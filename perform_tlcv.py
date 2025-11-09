59985d7a24614bb07daf8f21d96ff1155fbd9c4a
SebWae <s254120@dtu.dk>
2025-11-09 18:34:47 +0100

    feat: added hyperparam values to test

--- perform_tlcv.py
+++ perform_tlcv.py
@@ -1,23 +1,46 @@
 import pandas as pd
 import sklearn.linear_model as lm
 
-from utils import two_layer_cv, BaselineRegressor, ANNRegressor, Preprocessor
+from utils import two_layer_cv, BaselineRegressor, ANNRegressor, Preprocessor, BaselineClassifier, ANNClassifier
 
-models_list = [[BaselineRegressor()], 
-               [lm.Ridge(alpha=14)],
-               [lm.Ridge(alpha=15)],
-               ]
+# hyperparameter values to test
+hds = [1, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100]
+lams = [0.001, 0.005, 0.01, 0.05, 0.1, 0.5, 1, 5]
 
+# regression models to compare
+models_list_reg = [[ANNRegressor(hidden_dim=hd, verbose=False) for hd in hds],
+                   [BaselineRegressor()], 
+                   [lm.Ridge(alpha=a) for a in lams],
+                   ]
+
+# classification models to compare
+models_list_clf = [[ANNClassifier(hidden_dim=hd, verbose=False) for hd in hds],
+                   [BaselineClassifier()], 
+                   [lm.LogisticRegression(penalty="l2", C=1/lam) for lam in lams],
+                   ]
+
+# loading the data
 df = pd.read_csv("data/heartDisease.csv")
 
-PreProp = Preprocessor(task="regression")
+# preprocessing data
+PreProp = Preprocessor(task="classification")
 PreProp.fit(df)
 X, y = PreProp.transform(df)
 
-for models in models_list:
+# performing two-level cross-validation for the regression models
+for models in models_list_reg:
     two_layer_cv(k_in=10,
                 k_out=10,
                 models=models,
                 X=X,
                 y=y,
                 mode="regression")
+
+# performing two-level cross-validation for the classification models
+for models in models_list_clf:
+    two_layer_cv(k_in=10,
+                k_out=10,
+                models=models,
+                X=X,
+                y=y,
+                mode="classification")
