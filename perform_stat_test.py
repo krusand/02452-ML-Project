04e4fd5d12b6e6c66667f8b1369b2aea067766a5
krusand <110490348+krusand@users.noreply.github.com>
2025-11-11 20:26:46 +0100

    fix: input best parameters now support multiple best parameters

--- perform_stat_test.py
+++ perform_stat_test.py
@@ -16,18 +16,20 @@ classification_results_grouped = classification_results.groupby(["model", "param
 reg_ridge_lam_parameter = regression_results_grouped[(regression_results_grouped["model"] == "Ridge") & (regression_results_grouped["param_name"] == "lambda")]["param_val"].values[0]
 if np.array(reg_ridge_lam_parameter).shape != ():
     reg_ridge_lam_parameter = reg_ridge_lam_parameter[0]
+reg_ridge_lam_parameter = float(reg_ridge_lam_parameter)
 
 reg_ann_h_parameter = int(regression_results_grouped[(regression_results_grouped["model"] == "ANNRegressor") & (regression_results_grouped["param_name"] == "h")]["param_val"].values[0])
 if np.array(reg_ann_h_parameter).shape != ():
-    reg_ann_h_parameter = reg_ann_h_parameter[0]
+    reg_ann_h_parameter = int(reg_ann_h_parameter[0])
 
 clf_logistic_lam_parameter = classification_results_grouped[(classification_results_grouped["model"] == "LogisticRegression") & (classification_results_grouped["param_name"] == "lambda")]["param_val"].values[0]
 if np.array(clf_logistic_lam_parameter).shape != ():
     clf_logistic_lam_parameter = clf_logistic_lam_parameter[0]
+clf_logistic_lam_parameter = float(clf_logistic_lam_parameter)
 
 clf_ann_h_parameter = int(classification_results_grouped[(classification_results_grouped["model"] == "ANNClassifier") & (classification_results_grouped["param_name"] == "h")]["param_val"].values[0])
 if np.array(clf_ann_h_parameter).shape != ():
-    clf_ann_h_parameter = clf_ann_h_parameter[0]
+    clf_ann_h_parameter = int(clf_ann_h_parameter[0])
 
 
 
@@ -70,5 +72,5 @@ X_clf, y_clf = PreProp_clf.transform(df)
 # classification model comparisons
 model_comparisons_clf = [(log_reg_clf, ann_clf)]
 
-# for m1, m2 in model_comparisons_clf:
-#     p_val, lower, upper = performance_diff_test(mode="classification", model_1=m1, model_2=m2, X=X_clf, y=y_clf, seed=clf_cv_seed_used)
+for m1, m2 in model_comparisons_clf:
+    p_val, lower, upper = performance_diff_test(mode="classification", model_1=m1, model_2=m2, X=X_clf, y=y_clf, seed=clf_cv_seed_used)
