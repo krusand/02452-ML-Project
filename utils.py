906f2cd272d075300dd2de64369774c112e2bc71
krusand <110490348+krusand@users.noreply.github.com>
2025-11-11 19:50:16 +0100

    fix: baselines now output np arrays always

--- utils.py
+++ utils.py
@@ -325,7 +325,7 @@ class BaselineRegressor(BaseEstimator, RegressorMixin):
 
         check_is_fitted(self)
 
-        return np.repeat(self.baseline_value, X.shape[0])
+        return np.array(np.repeat(self.baseline_value, X.shape[0]))
 
     def get_feature_names_out(self, *args, **params):
         return self.columns_
@@ -350,7 +350,7 @@ class BaselineClassifier(BaseEstimator, ClassifierMixin):
 
         check_is_fitted(self)
 
-        return np.repeat(self.baseline_value, X.shape[0])
+        return np.array(np.repeat(self.baseline_value, X.shape[0]))
 
     def get_feature_names_out(self, *args, **params):
         return self.columns_
@@ -361,7 +361,7 @@ class BaselineClassifier(BaseEstimator, ClassifierMixin):
 
 class ANNRegressor(BaseEstimator, RegressorMixin):
     def __init__(
-        self, hidden_dim, input_dim=10, n_epochs=300, learning_rate=1e-5, verbose=True
+        self, hidden_dim, input_dim=10, n_epochs=2500, learning_rate=1e-5, verbose=True
     ):
         self.hidden_dim_ = hidden_dim
         self.input_dim_ = input_dim
@@ -429,7 +429,7 @@ class ANNRegressor(BaseEstimator, RegressorMixin):
 
 class ANNClassifier(BaseEstimator, ClassifierMixin):
     def __init__(
-        self, hidden_dim, input_dim=9, n_epochs=300, learning_rate=1e-5, verbose=True
+        self, hidden_dim, input_dim=9, n_epochs=2500, learning_rate=1e-5, verbose=True
     ):
         self.hidden_dim_ = hidden_dim
         self.input_dim_ = input_dim
